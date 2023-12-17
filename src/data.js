fc53db7bf853c0310e105a34a6dba9f2dcb6ede0
saim1123 <saimtahir3211@gmail.com>
2023-12-18 01:58:27 +0500

    build the local data

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    id: 1,
+    name: "Lelah Nichols",
+    img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+    skills: ["Photoshop", "Illustrator"],
+  },
+  {
+    id: 2,
+    name: "Weiss John",
+    img: "https://images.unsplash.com/photo-1580489944761-15a19d654956?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
+    skills: ["Photoshop", "Illustrator"],
+  },
+  {
+    id: 3,
+    name: "Robert Morin",
+    img: "https://plus.unsplash.com/premium_photo-1664536392896-cd1743f9c02c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
+    skills: ["Photoshop", "Illustrator"],
+  },
+  {
+    id: 4,
+    name: "Anne Cambel",
+    img: "https://images.unsplash.com/photo-1614880353165-e56fac2ea9a8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODJ8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
+    skills: ["Photoshop", "Illustrator"],
+  },
+  {
+    id: 5,
+    name: "Frank Flores",
+    img: "https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
+    skills: ["Photoshop", "Illustrator"],
+  },
+  {
+    id: 6,
+    name: "Zayn Khan",
+    img: "https://plus.unsplash.com/premium_photo-1678197937465-bdbc4ed95815?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
+    skills: ["Photoshop", "Illustrator"],
+  },
+];
+
+export default data;
