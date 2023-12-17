697e6fd060d1a595a2db395f007d5f4836423e03
saim1123 <saimtahir3211@gmail.com>
2023-12-18 02:08:28 +0500

    Add some styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import List from "./components/List";
+import SearchPerson from "./components/SearchPerson";
 
 function App() {
-  return <div></div>;
+  return (
+    <main>
+      <h1>Users</h1>
+      <SearchPerson />
+      <List />
+    </main>
+  );
 }
 
 export default App;
